ceda905533fb39a69b22c467cdaa1c2236f3d468
Phantem717 <pradahsuherli@gmail.com>
2024-11-13 15:32:39 +0700

    Git Init Commit

---
+++ Final_Project_OOAD/src/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Final_Project_OOAD/src/module-info.java
@@ -0,0 +1,6 @@
+module Final_Project_OOAD {
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.base;
+	opens Main;
+}
